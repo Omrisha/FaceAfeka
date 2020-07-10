8a80f18d4ee12e2faeb68ced4eec54130f22728d
Omri Shapira <omri.shapira@gmail.com>
2020-07-10 16:47:52 +0300

    Adding prod DB credentials

--- functions.php
+++ functions.php
@@ -1,11 +1,16 @@
 <?php
     session_start();
+    //mysql://b1bee8946f0524:bdd81574@us-cdbr-east-02.cleardb.com/heroku_01d7bd2902d3fbf?reconnect=true
     function db_connect() {
         global $conn; // db connection variable
-        $db_server = "localhost";
-        $username = "root";
-        $password = "root";
-        $db_name = "faceafeka";
+        //$db_server = "localhost";
+        $db_server = "us-cdbr-east-02.cleardb.com";
+        //$username = "root";
+        $username = "b1bee8946f0524";
+        //$password = "root";
+        $password = "bdd81574";
+        //$db_name = "faceafeka";
+        $db_name = "heroku_01d7bd2902d3fbf";
         // create a connection
         $conn = new mysqli($db_server, $username, $password, $db_name);
         // check connection for errors
