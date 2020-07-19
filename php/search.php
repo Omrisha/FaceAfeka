c660f770b2ce6a80c8ec6229d832a3be9d538c21
Omri Shapira <omri.shapira@gmail.com>
2020-07-19 10:21:58 +0300

    fix bug in adding a friend

--- php/search.php
+++ php/search.php
@@ -4,12 +4,12 @@
     
     $output = '';
     if ($_GET['query'] == "*"){
-      $sql = "SELECT id, username, (SELECT COUNT(*) FROM friends WHERE friends.user_id = users.id AND friends.friend_id = {$_SESSION['user_id']}) AS is_friend
+      $sql = "SELECT id, username, first_name, last_name, (SELECT COUNT(*) FROM friends WHERE friends.user_id = users.id AND friends.friend_id = {$_SESSION['user_id']}) AS is_friend
             FROM users 
             WHERE id != {$_SESSION['user_id']}
             HAVING is_friend = 0";
     } else {
-      $sql = "SELECT id, username, (SELECT COUNT(*) FROM friends WHERE friends.user_id = users.id AND friends.friend_id = {$_SESSION['user_id']}) AS is_friend
+      $sql = "SELECT id, username, first_name, last_name, (SELECT COUNT(*) FROM friends WHERE friends.user_id = users.id AND friends.friend_id = {$_SESSION['user_id']}) AS is_friend
               FROM users 
               WHERE username LIKE '%{$_GET['query']}%' AND id != {$_SESSION['user_id']}
               HAVING is_friend = 0";
@@ -19,7 +19,7 @@
       $output .= '<ul>';
       while($row=$result->fetch_assoc())
       {
-        $output .= "<li><a href='php/profile.php?username={$row['username']}'>{$row['username']}</a></li>";
+        $output .= "<li><a href='php/profile.php?username={$row['username']}'>{$row['first_name']} {$row['last_name']}</a></li>";
         $output .= "<li><a href='php/add-friend.php?uid={$row['id']}'>[add]</a></li>";
       }
       $output .= '</ul>';
